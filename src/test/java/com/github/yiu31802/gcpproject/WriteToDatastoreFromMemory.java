0acd23b287bcf381d974567c33f11d8a0830635e
Junji Shimagaki <yiu31802@nifty.com>
2018-01-05 11:31:35 +0900

    Execute a test class
    Link about how to execute a test class:
      https://stackoverflow.com/questions/4060613/
      from-maven-how-do-i-run-a-class-that-lives-under-src-test-java

---
+++ src/test/java/com/github/yiu31802/gcpproject/WriteToDatastoreFromMemory.java
@@ -0,0 +1,7 @@
+package com.github.yiu31802.gcpproject;
+
+public class WriteToDatastoreFromMemory {
+    public static void main(String[] args) {
+        System.out.print("here we are:\n");
+    }
+}
