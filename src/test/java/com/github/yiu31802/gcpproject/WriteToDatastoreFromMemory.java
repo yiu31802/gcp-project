23f59b8159dabe2d14d3e53104becc091a03df21
Junji Shimagaki <yiu31802@nifty.com>
2018-01-05 11:52:39 +0900

    Get an input -DXGCPID

--- src/test/java/com/github/yiu31802/gcpproject/WriteToDatastoreFromMemory.java
+++ src/test/java/com/github/yiu31802/gcpproject/WriteToDatastoreFromMemory.java
@@ -2,6 +2,7 @@ package com.github.yiu31802.gcpproject;
 
 public class WriteToDatastoreFromMemory {
     public static void main(String[] args) {
-        System.out.print("here we are:\n");
+        String project_id = (System.getProperty("XGCPID") != null) ? (System.getProperty("XGCPID")) : "";
+        System.out.print("here we are:\n" + project_id);
     }
 }
